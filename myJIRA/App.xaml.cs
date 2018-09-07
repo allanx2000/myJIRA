da4fa65cac5edc1477a9c7c05a3245e6d9a2b725
Allan <allan.xiao@gmail.com>
2018-09-06 23:55:02 -0400

    Add Single instance logic

--- myJIRA/App.xaml.cs
+++ myJIRA/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Innouvous.Utils.SingleInstance;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Data;
@@ -13,5 +14,10 @@ namespace myJIRA
     /// </summary>
     public partial class App : Application
     {
+        private void Application_Startup(object sender, StartupEventArgs e)
+        {
+            if (Checker.AlreadyRunning)
+                Environment.Exit(0);
+        }
     }
 }
